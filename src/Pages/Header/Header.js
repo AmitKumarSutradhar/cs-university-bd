c13a0dca57059858449f843a033fca515bb3fc83
Amit Kumar Sutradhar <amit596450@gmail.com>
2022-10-27 14:07:47 +0600

    Navbar Basic Setup

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Button, Container, Image, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/UserContext';
 
 const Header = () => {
+
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.error(error))
+    }
+
     return (
         <div>
-            hello header
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand><Link><Image src='logo.png'></Image></Link></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link><Link to='/courses'>Courses</Link></Nav.Link>
+                            <Nav.Link><Link to='/faq'>FAQ</Link></Nav.Link>
+                            <Nav.Link><Link to='/blog'>Blog</Link></Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Toggle</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Toggle</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <>
+                                {
+                                    user?.uid ?
+                                        <>
+                                            <span>{user?.displayName}</span>
+                                            <Button variant="light" onClick={handleLogOut}>Log out</Button>
+                                        </>
+                                        :
+                                        <>
+                                            <Link to='/login'>Login</Link>
+                                            <Link to='/register'>Register</Link>
+                                        </>
+                                }
+
+
+                            </>
+                            <Link to="/profile">
+                                {user?.photoURL ?
+                                    <Image
+                                        style={{ height: '30px' }}
+                                        roundedCircle
+                                        src={user?.photoURL}>
+                                    </Image>
+                                    : <>User</>
+                                }
+                            </Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
